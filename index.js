eb1f239856f948895c04e8ab6d941d477657f8ae
Guilherme dos Santos <guilhermesantospineda@gmail.com>
2023-03-19 00:06:42 -0300

    Quinto commit

--- index.js
+++ index.js
@@ -6,15 +6,75 @@ var conteudo = document.querySelector(".conteudo")
 var conteudo2 = document.querySelector(".conteudo2")
 var youPick = document.querySelector(".youPick")
 var theHousePicked = document.querySelector(".theHousePicked")
+var mensagemEPlayAgain = document.querySelector(".mensagemEPlayAgain")
+var mesage = document.querySelector(".mesage")
+var playAgain
 var paper = document.querySelector(".paper")
 var rock = document.querySelector(".rock")
 var scissor = document.querySelector(".scissor")
 var x = document.querySelector(".x")
+var numero = document.querySelector(".numero")
 var a = 0
+var score = 0
 var result
 var suaVez
+var vezDaMaquina
 
+function verificarVencedor(){
+  if(suaVez == 'bordaTres' && vezDaMaquina == 'papel'){
+    console.log('Voce selecionou Tesoura')
+    console.log('Voce ganhou')
+    mesage.innerText = 'YOU WIN'
+    score++
+    numero.innerText = `${score}`
+  }
+  if(suaVez == 'bordaTres' && vezDaMaquina == 'pedra'){
+    console.log('Voce selecionou Tesoura')
+    console.log('Voce Perdeu')
+    mesage.innerText = 'YOU LOSE'
+  }
+  if(suaVez == 'bordaTres' && vezDaMaquina == 'tesoura'){
+    console.log('Voce selecionou Tesoura')
+    console.log('Voce empatou')
+    mesage.innerText = 'EMPATE'
+  }
 
+  if(suaVez == 'bordaDois' && vezDaMaquina == 'tesoura'){
+    console.log('Voce selecionou Pedra')
+    console.log('Voce ganhou')
+    mesage.innerText = 'YOU WIN'
+    score++
+    numero.innerText = `${score}`
+  }
+  if(suaVez == 'bordaDois' && vezDaMaquina == 'papel'){
+    console.log('Voce selecionou Pedra')
+    console.log('Voce perdeu')
+    mesage.innerText = 'YOU LOSE'
+  }
+  if(suaVez == 'bordaDois' && vezDaMaquina == 'pedra'){
+    console.log('Voce selecionou Pedra')
+    console.log('Voce empatou')
+    mesage.innerText = 'EMPATE'
+  }
+
+  if(suaVez == 'bordaUm' && vezDaMaquina == 'pedra'){
+    console.log('Voce selecionou Papel')
+    console.log('Voce ganhou')
+    mesage.innerText = 'YOU WIN'
+    score++
+    numero.innerText = `${score}`
+  }
+  if(suaVez == 'bordaUm' && vezDaMaquina == 'tesoura'){
+    console.log('Voce selecionou Papel')
+    console.log('Voce perdeu')
+    mesage.innerText = 'YOU LOSE'
+  }
+  if(suaVez == 'bordaUm' && vezDaMaquina == 'papel'){
+    console.log('Voce selecionou Papel')
+    console.log('Voce empatou')
+    mesage.innerText = 'EMPATE'
+  }
+}
 // GERAR NUMERO COMEÇO
 function gerarNumero(){
     let nr = 100
@@ -60,7 +120,22 @@ botoes.forEach(e =>{
     e.addEventListener('click',(c)=>{
         console.log(gerarNumero())
         c = mudarTela(e)
-
+        mensagemEPlayAgain.style.display = 'none'
+        setTimeout(()=>{
+          
+          mensagemEPlayAgain.innerHTML=`
+          <div class="mesage"></div>
+          <div class="playAgain" onclick="jogarNovamente()"> PLAY AGAIN </div>
+          `
+          setTimeout(()=>{
+            mesage = document.querySelector(".mesage")
+            verificarVencedor()
+          },100)
+          
+          
+          mensagemEPlayAgain.style.display = 'flex'
+        },2700)
+        playAgain = document.querySelector(".playAgain")
         if(result >= 67 && result <= 100){
           hole()
             setTimeout(()=>{
@@ -71,8 +146,10 @@ botoes.forEach(e =>{
                   <img class="icone2" src="./images/icon-scissors.svg" alt="icon">
                 </div>
               `
-            },2000)
+              vezDaMaquina = 'tesoura'
+            },1200)
             console.log("tesoura")
+            
         }
 
         if(result >= 34 && result <=66){
@@ -85,7 +162,8 @@ botoes.forEach(e =>{
                 <img class="icone2" src="./images/icon-paper.svg" alt="icon">
               </div>
             `
-          },2000)
+            vezDaMaquina = 'papel'
+          },1200)
             console.log("papel")
         }
 
@@ -99,7 +177,8 @@ botoes.forEach(e =>{
                 <img class="icone2" src="./images/icon-rock.svg" alt="icon">
               </div>
             `
-          },2000)
+            vezDaMaquina = 'pedra'
+          },1200)
             console.log("pedra")
         }
     })
@@ -111,10 +190,14 @@ botoes.forEach(e =>{
 function mudarTela(i){
     if(a == 1){
         console.log(i)
-        suaVez = i
+        suaVez = i.classList[0]
         conteudo.style.display = 'none'
         conteudo2.style.display = 'flex'
     }
+}
+function jogarNovamente(){
+  conteudo.style.display = 'flex'
+  conteudo2.style.display = 'none' 
 }
 // APARECER E SUMIR REGRAS COMEÇO
 regras.addEventListener("click", ()=>{
